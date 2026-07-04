8ab048ac6e500d9ca43692c2dea9fb684b1ed3a4
Niki Mardari <nikitamardari@gmail.com>
2026-07-04 23:34:11 +0100

    Removed old Web directory

--- Web_old/database/init.sql
+++
@@ -1,6 +0,0 @@
--- Optional database notes for SatCache.
--- The FastAPI backend creates these tables automatically on startup.
--- This file is mounted into the PostgreSQL container for future migrations/seeding.
-
--- You can open the database from the host with:
--- docker exec -it satcache_db psql -U satcache_user -d satcache_db
